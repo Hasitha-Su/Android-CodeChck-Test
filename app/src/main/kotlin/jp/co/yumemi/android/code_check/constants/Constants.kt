93ad351657b45fa5833db5ae3a0373ee264449b0
Hasitha-Su <hasithakms@gmail.com>
2023-06-18 23:00:54 +0530

    Update comment in constant file

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/constants/Constants.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/constants/Constants.kt
@@ -4,7 +4,6 @@ package jp.co.yumemi.android.code_check.constants
  * A utility object that holds constant values used in the application.
  *
  * This object contains various constant properties related to the application, such as URLs and paths.
- *
  * @object Constants
  */
 object Constants {
