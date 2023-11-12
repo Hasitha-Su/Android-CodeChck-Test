b306f7018d676e4cccb4ae74799fb2aa477dc782
Hasitha-KMS <hasithakms@absi.digital>
2023-11-12 23:50:52 +0530

    Implement HiltTestActivity for Dependency Injection in Tests

---
+++ app/src/test/kotlin/jp/co/yumemi/android/code_check/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package jp.co.yumemi.android.code_check
+
+import androidx.fragment.app.FragmentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : FragmentActivity()
